f4be3c6afc010fce7f587bbd8e125428d0bcd1fc
jaures kano <ruddyjaures@gmail.com>
2020-11-12 14:19:53 +0100

    evolution : template de base et l'insertion de adminlte comme support de design avec bootstrap

--- assets/base/index.js
+++ assets/base/index.js
@@ -2,6 +2,8 @@ import 'jquery'
 import './css/base.scss'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import '@fortawesome/fontawesome-free/js/all.min'
+import 'overlayscrollbars/js/jquery.overlayScrollbars'
+import './js/adminlte'
 
 (function (window, $) {
 
