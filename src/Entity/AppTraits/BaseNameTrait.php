a337940baac132e8d2a4a5c776e733f68ee85ade
jaures kano <ruddyjaures@gmail.com>
2020-11-16 11:14:11 +0100

    fix : resolution des problemes de versionning

--- src/Entity/AppTraits/BaseNameTrait.php
+++ src/Entity/AppTraits/BaseNameTrait.php
@@ -24,45 +24,30 @@ trait BaseNameTrait
     private $prenom;
 
 
-<<<<<<< HEAD
-    public function getNom (): ?string
-=======
+
     public function getNom(): ?string
->>>>>>> dev-front
     {
         return $this->nom;
     }
 
-<<<<<<< HEAD
-    public function setNom (string $nom): self
-=======
+
     public function setNom(string $nom): self
->>>>>>> dev-front
     {
         $this->nom = $nom;
 
         return $this;
     }
 
-<<<<<<< HEAD
-    public function getPrenom (): ?string
-=======
     public function getPrenom(): ?string
->>>>>>> dev-front
     {
         return $this->prenom;
     }
 
-<<<<<<< HEAD
-    public function setPrenom (?string $prenom): self
-=======
     public function setPrenom(?string $prenom): self
->>>>>>> dev-front
     {
         $this->prenom = $prenom;
 
         return $this;
     }
-
 
 }
